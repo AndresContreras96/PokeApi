c171a5181b4d6fdade0e31d62f666418d565aaba
AndresContreras96 <contreras9996@gmail.com>
2024-05-06 13:04:43 -0600

    pokemon details component

--- src/Pages/Details/Details.tsx
+++ src/Pages/Details/Details.tsx
@@ -3,8 +3,9 @@ import { useGetPokemonQuery } from "../../app/services/pokemonApi";
 import PokemonView from "../../components/PokemonView/PokemonView";
 import { useNavigate, useParams } from "react-router-dom";
 import Layout from "../../components/Layout/Layout";
+import PokemonDetails from "./components/PokemonDetails/PokemonDetails";
 import "../Details/Details.css";
-import ProgressBar from "../../components/ProgressBar/ProgressBar";
+
 
 const Details = () => {
   let { name } = useParams();
@@ -27,50 +28,10 @@ const Details = () => {
         <button className="button" onClick={handleGoBack}>
           Go back
         </button>
-        {isFetching && <div>...Loading</div>}
-        {data && (
-          <div className="main-container">
-            <div className="type">
-              <h2>Type</h2>
-              <span>
-                {data &&
-                  data.types
-                    .map((el) => {
-                      return el.type.name;
-                    })
-                    .join("/")}
-              </span>
-            </div>
-            <div className="info">
-              <div>Number:{data.id} </div>
-              <div>Name: {data.name} </div>
-              <div>Height: {data.height}</div>
-              <div>Weight: {data.weight}</div>
-            </div>
-            <div className="stadistics">
-              <div className="stats">
-                <h2>Stats</h2>
-                {data.stats.map((el) => {
-                  return (
-                    <div key={el.stat.name} className="stat-container">
-                      <span>{el.stat.name}</span>
-                      <div style={{ display: "flex" }}>
-                        <span>{el["base_stat"]}</span>
-                        <ProgressBar value={el["base_stat"]} maxValue={255} />
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
-              <div className="abilities">
-                <h2>Abilities</h2>
-                {data.abilities.map((el) => {
-                  return <div key={el.ability.name}>{el.ability.name}</div>;
-                })}
-              </div>
-            </div>
-          </div>
+        {isFetching && (
+          <div style={{ textAlign: "center", width: "100%" }}>...Loading</div>
         )}
+        {data && <PokemonDetails data={data} />}
       </div>
     </Layout>
   );
---
+++ src/Pages/Details/components/PokemonDetails/PokemonDetails.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Pokemon } from "../../../../app/types/Pokemon";
+import ProgressBar from "../../../../components/ProgressBar/ProgressBar";
+import "../PokemonDetails/PokemonDetails.css";
+
+interface Props {
+  data: Pokemon;
+}
+
+const PokemonDetails: React.FC<Props> = ({ data }) => {
+  return (
+    <div className="pokemon-details-container">
+      <div className="type">
+        <h2>Type</h2>
+        <span>
+          {data &&
+            data.types
+              .map((el) => {
+                return el.type.name;
+              })
+              .join("/")}
+        </span>
+      </div>
+      <div className="info">
+        <div>Number:{data.id} </div>
+        <div>Name: {data.name} </div>
+        <div>Height: {data.height}</div>
+        <div>Weight: {data.weight}</div>
+      </div>
+      <div className="stadistics">
+        <div className="stats">
+          <h2>Stats</h2>
+          {data.stats.map((el) => {
+            return (
+              <div key={el.stat.name} className="stat-container">
+                <span>{el.stat.name}</span>
+                <div style={{ display: "flex" }}>
+                  <span>{el["base_stat"]}</span>
+                  <ProgressBar value={el["base_stat"]} maxValue={255} />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="abilities">
+          <h2>Abilities</h2>
+          {data.abilities.map((el) => {
+            return <div key={el.ability.name}>{el.ability.name}</div>;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PokemonDetails;
